0603aa9376b58870b8fb8f4ddb125c6d6505ab6a
Ivan Perez <ivan.perez002@gmail.com>
2019-09-11 03:13:28 +0000

    completed lesson 22

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_030809) do
+ActiveRecord::Schema.define(version: 2019_09_11_012951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_06_030809) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "lessons", force: :cascade do |t|
