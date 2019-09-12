3e9f0904c0a3ec6237bd76e182701a8048163127
Ivan Perez <ivan.perez002@gmail.com>
2019-09-12 06:46:48 +0000

    Completed lesson 29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment, only: [:show]
+
   def show
   end
 
   private
+
+  def require_enrollment
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "Must be enrolled in course to view lesson - #{current_lesson.title}"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
