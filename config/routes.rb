daa0b5c7442860207090c6fe0a7adbba36c51fcb
Ivan Perez <ivan.perez002@gmail.com>
2019-09-20 03:28:44 +0000

    completed lesson 39

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'careers', to: 'static_pages#careers'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-
   resources :lessons, only: [:show]
-
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
